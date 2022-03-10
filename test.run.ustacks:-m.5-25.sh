3471c033e83e2ff90b8b82cea1c1ec8f563fcbeb
Greentea Dog <68783153+Tom1Ru@users.noreply.github.com>
2022-03-10 11:31:55 +0900

    Update test.run.ustacks:-m.5-25.sh

--- test.run.ustacks:-m.5-25.sh
+++ test.run.ustacks:-m.5-25.sh
@@ -4,10 +4,10 @@ set -u
 set -o pipefail
 
 #denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O ./popmap.Flash.txt --samples ./raw -o stacks_9
-#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O ./popmap.Flash.txt --samples ./raw -o stacks_10
-#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O ./popmap.Flash.txt --samples ./raw -o stacks_11
-#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O ./popmap.Flash.txt --samples ./raw -o stacks_12
-#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O ./popmap.Flash.txt --samples ./raw -o stacks_13
+#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 10" -O ./popmap.Flash.txt --samples ./raw -o stacks_10
+#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 15" -O ./popmap.Flash.txt --samples ./raw -o stacks_11
+#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 20" -O ./popmap.Flash.txt --samples ./raw -o stacks_12
+#denovo_map.pl -M 5 -T 30 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 25" -O ./popmap.Flash.txt --samples ./raw -o stacks_13
 
 /home/t20558at/bin/anaconda3/bin/denovo_map.pl -M 5 -T 7 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 5" -O /home/t20558at/hotaru/popmap.Flash.txt --samples /home/t20558at/hotaru/raw -o /home/t20558at/hotaru/stacks_9 &
 /home/t20558at/bin/anaconda3/bin/denovo_map.pl -M 5 -T 7 -S -b 0 -X "populations:--write_single_snp -R 0.3 --vcf --phylip --phylip-var --fasta-loci --genepop" -X "ustacks:-m 10" -O /home/t20558at/hotaru/popmap.Flash.txt --samples /home/t20558at/hotaru/raw -o /home/t20558at/hotaru/stacks_10 &
